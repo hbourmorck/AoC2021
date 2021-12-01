685fd1311a230f0437d696c42513aaa1ed981ee9
Henri Bourmorck <henri.bourmorck@iba-group.com>
2021-12-01 07:52:34 +0100

    First day solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.zipWithNext().count { it.second > it.first }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.mapIndexed { index, i ->
+            if (index + 3 <= input.size)
+                i + input[index + 1].toInt() + input[index + 2].toInt()
+            else
+                Int.MIN_VALUE
+        }.zipWithNext().count { it.second > it.first }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
