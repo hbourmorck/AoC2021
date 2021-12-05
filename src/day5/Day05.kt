80be444cfac054c56d4c0b9df5490602d3140d0f
Henri Bourmorck <hbourmo@gmail.com>
2021-12-05 07:56:13 +0100

    Day 5 solution, simplification

--- src/day5/Day05.kt
+++ src/day5/Day05.kt
@@ -6,30 +6,27 @@ import kotlin.math.abs
 fun main() {
     fun getLines(input:List<String>): List<List<Pair<Int, Int>>>
     {
-        return input.map { it.split(" -> ").map { Pair(it.split(",")[0].toInt(), it.split(",")[1].toInt()) } }
+        return input.map { it.split(" -> ").map { coordinate -> Pair(coordinate.split(",")[0].toInt(), coordinate.split(",")[1].toInt()) } }
     }
 
-    fun part1(input: List<String>): Int {
-        val lines = getLines(input)
-        val verticalMotion = lines.filter { it[0].first == it[1].first }.toList()
-        val horizontalMotion = lines.filter { it[0].second == it[1].second }.toList()
+    fun verticalMotion(verticalMotions: List<List<Pair<Int, Int>>>, points: MutableMap<Pair<Int,Int>, Int>)
+    {
+        verticalMotions.forEach {
+        val startPoint = it[0].second
+        val endPoint = it[1].second
+        val xPoint = it[0].first
+        val direction = if (startPoint < endPoint) startPoint..endPoint else endPoint..startPoint
 
-        var points: MutableMap<Pair<Int,Int>, Int> = mutableMapOf()
-
-        verticalMotion.forEach {
-            val startPoint = it[0].second
-            val endPoint = it[1].second
-            val xPoint = it[0].first
-            val direction = if (startPoint < endPoint) startPoint..endPoint else endPoint..startPoint
-
-            direction
-                .forEach { i ->
-                    points[Pair(xPoint,i)] = (points[Pair(xPoint, i)]?:0) + 1
-                }
-
+        direction
+            .forEach { i ->
+                points[Pair(xPoint,i)] = (points[Pair(xPoint, i)]?:0) + 1
+            }
         }
+    }
 
-        horizontalMotion.forEach {
+    fun horizontalMotion(horizontalMotions: List<List<Pair<Int, Int>>>, points: MutableMap<Pair<Int,Int>, Int>)
+    {
+        horizontalMotions.forEach {
             val startPoint = it[0].first
             val endPoint = it[1].first
             val yPoint = it[0].second
@@ -41,7 +38,15 @@ fun main() {
                 }
 
         }
+    }
 
+    fun part1(input: List<String>): Int {
+        val lines = getLines(input)
+        val verticalMotions = lines.filter { it[0].first == it[1].first }
+        val horizontalMotions = lines.filter { it[0].second == it[1].second }
+        val points: MutableMap<Pair<Int,Int>, Int> = mutableMapOf()
+        verticalMotion(verticalMotions, points)
+        horizontalMotion(horizontalMotions, points)
         return points.count{
             it.value >= 2
         }
@@ -49,65 +54,22 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         val lines = getLines(input)
-        val verticalMotion = lines.filter { it[0].first == it[1].first }.toList()
-        val horizontalMotion = lines.filter { it[0].second == it[1].second }.toList()
+        val verticalMotions = lines.filter { it[0].first == it[1].first }
+        val horizontalMotions = lines.filter { it[0].second == it[1].second }
         val diagonalMotion = lines.filter { abs(it[0].first - it[1].first) == abs(it[0].second - it[1].second) }
-
-        var points: MutableMap<Pair<Int,Int>, Int> = mutableMapOf()
-
-        verticalMotion.forEach {
-            val startPoint = it[0].second
-            val endPoint = it[1].second
-            val xPoint = it[0].first
-            val direction = if (startPoint < endPoint) startPoint..endPoint else endPoint..startPoint
-
-            direction
-                .forEach { i ->
-                    points[Pair(xPoint,i)] = (points[Pair(xPoint, i)]?:0) + 1
-                }
-
-        }
-
-        horizontalMotion.forEach {
-            val startPoint = it[0].first
-            val endPoint = it[1].first
-            val yPoint = it[0].second
-            val direction = if (startPoint < endPoint) startPoint..endPoint else endPoint..startPoint
-
-            direction
-                .forEach { i ->
-                    points[Pair(i,yPoint)] = (points[Pair(i, yPoint)]?:0) + 1
-                }
-
-        }
-
+        val points: MutableMap<Pair<Int,Int>, Int> = mutableMapOf()
+        verticalMotion(verticalMotions, points)
+        horizontalMotion(horizontalMotions, points)
         diagonalMotion.forEach {
             val startXPoint = it[0].first
             val endXPoint = it[1].first
             val startYPoint = it[0].second
             val endYPoint = it[1].second
-
             val distance = abs(startXPoint - endXPoint)
             for(i in 0..distance)
             {
-                var x = startXPoint
-                var y = startYPoint
-                if(startXPoint < endXPoint)
-                {
-                    x += i
-                }
-                else
-                {
-                    x -= i
-                }
-                if(startYPoint < endYPoint)
-                {
-                    y += i
-                }
-                else
-                {
-                    y -= i
-                }
+                val x = startXPoint + if(startXPoint < endXPoint) i else -i
+                val y = startYPoint + if(startYPoint < endYPoint) i else -i
                 points[Pair(x,y)] = (points[Pair(x, y)]?:0) + 1
             }
 
